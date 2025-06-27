8431172c5c0b0fbe625d226801f9ba60720f0c56
ennbou <ennbou@yandex.com>
2025-06-27 16:06:00 +0100

    add tuist inspect

--- Project.swift
+++ Project.swift
@@ -1,27 +1,56 @@
 import ProjectDescription
 
+let TARGETNAME = "AIChat"
+
 let project = Project(
-    name: "AIChat",
-    settings: .settings(configurations: [
-        .debug(name: "Debug", xcconfig: "./xcconfigs/AIChat-Project.xcconfig"),
-        .release(name: "Release", xcconfig: "./xcconfigs/AIChat-Project.xcconfig"),
-    ]),
-    targets: [
-        .target( 
-            name: "AIChat", 
-            destinations: .macOS, 
-            product: .app,
-            bundleId: "com.renault.AIChat",
-            sources: ["AIChat/**"],
-            scripts: [
-                .pre(script: "git diff --name-only HEAD | grep '\\.swift$' | xargs -0 xcrun swift-format format -i", name: "Swift Format"),
-                .pre(script: "git diff --name-only HEAD | grep '\\.swift$' | xargs -n1 swiftlint lint --path", name: "Swift Lint")
-            ],
-            dependencies: [],
-            settings: .settings(configurations: [ 
-                .debug(name: "Debug", xcconfig: "./xcconfigs/AIChat.xcconfig"), 
-                .debug(name: "Release", xcconfig: "./xcconfigs/AIChat.xcconfig"), 
-            ]) 
-        ), 
-    ]
+  name: TARGETNAME,
+  settings: .settings(configurations: [
+    .debug(name: "Debug", xcconfig: "./xcconfigs/AIChat-Project.xcconfig"),
+    .release(name: "Release", xcconfig: "./xcconfigs/AIChat-Project.xcconfig"),
+  ]),
+  targets: [
+    .target(
+      name: TARGETNAME,
+      destinations: .macOS,
+      product: .app,
+      bundleId: "com.renault.AIChat",
+      sources: ["AIChat/**"],
+      scripts: [
+        .pre(
+          script:
+            "git diff --name-only HEAD | grep '\\.swift$' | xargs -n1 xcrun swift-format format -i",
+          name: "Swift Format"
+        ),
+        .pre(
+          script: "git diff --name-only HEAD | grep '\\.swift$' | xargs -n1 swiftlint lint",
+          name: "Swift Lint"
+        ),
+      ],
+      dependencies: [],
+      settings: .settings(configurations: [
+        .debug(name: "Debug", xcconfig: "./xcconfigs/AIChat.xcconfig"),
+        .debug(name: "Release", xcconfig: "./xcconfigs/AIChat.xcconfig"),
+      ])
+    )
+  ],
+  schemes: [
+    .scheme(
+      name: TARGETNAME,
+      shared: true,
+      buildAction: .buildAction(
+        targets: ["AIChat"],
+        postActions: [
+          .executionAction(
+            title: "Inspect Build",
+            scriptText: """
+              eval \"$($HOME/.local/bin/mise activate -C $SRCROOT bash --shims)\"
+              tuist inspect build
+              """
+          )
+        ],
+        runPostActionsOnFailure: true
+      ),
+      runAction: .runAction(configuration: "Debug")
+    )
+  ]
 )
